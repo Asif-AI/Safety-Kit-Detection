d471d31af315b7f53a29282d6b937c68b660c244
asif <aasifkhan02@gmail.com>
2024-07-01 00:43:58 +0530

    Basic Setup and Folder Structure Created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'isd',
+    version= '0.0.0',
+    author= 'Asif-AI',
+    author_email= 'aasifkhan02@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
